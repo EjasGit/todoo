fda553132b3a8f769d8eab399729127c364ca953
ejas <ejas01@outlook.com>
2024-05-25 17:41:27 +0530

    search box

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -12,15 +12,7 @@ class Home extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.symmetric(horizontal: 15),
         child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: TextField(),
-            )
-          ],
+          children: [searchBox()],
         ),
       ),
     );
@@ -46,6 +38,40 @@ class Home extends StatelessWidget {
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class searchBox extends StatelessWidget {
+  const searchBox({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: const TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: tdBlack,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(
+            maxHeight: 20,
+            minWidth: 25,
+          ),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGrey),
+        ),
       ),
     );
   }
